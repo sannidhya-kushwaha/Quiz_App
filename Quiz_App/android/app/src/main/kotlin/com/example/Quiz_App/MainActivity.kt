3eccda65aca4f437d4d08dc57f17ecf0b55e140e
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-08 18:01:20 +0530

    Minor Changes

---
+++ Quiz_App/android/app/src/main/kotlin/com/example/Quiz_App/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Quiz_App
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Quiz_App/lib/result.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  //hello
+  int get myresultNum {
+    int resultscoreNum;
+    if (resultScore == resultScore) {
+      resultscoreNum = resultScore;
+    } else {
+      resultscoreNum = resultScore;
+    }
+    return resultscoreNum;
+  }
+  //hello end
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Text('Your Score is ' + myresultNum.toString()),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
